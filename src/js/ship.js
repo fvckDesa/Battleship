dc73be68da60f5b925b24027a9cf29f202b9bdc4
Luca De Santis <lucaroma2705@gmail.com>
2022-06-29 17:44:24 +0200

    ship factory function

--- src/js/ship.js
+++ src/js/ship.js
@@ -1,25 +1,45 @@
+
+
+/**
+ * It creates a new object with a length property, a hitPoints property, a hit method, and an isSunk
+ * method.
+ * 
+ * The length property is read-only and is set to the length argument passed to the Ship function.
+ * 
+ * The hitPoints property is read-only and is set to an array of length length, filled with false
+ * values.
+ * 
+ * The hit method takes a number as an argument and sets the value of the hitPoints array at that index
+ * to true.
+ * 
+ * The isSunk method returns true if every value in the hitPoints array is true, and false otherwise.
+ * @param length - The length of the ship. Must be between 1 and 5.
+ * @returns An object with the properties length, hitPoints, hit, and isSunk.
+ */
 function Ship(length) {
-    if(length < 2 || length > 5) {
-        throw new Error("ship length must be between 2 and 5");
+    if(length > 5 || length < 1) {
+        throw new Error("Length of the ship must be between 1 and 5");
     }
+    const hitPoints = new Array(length).fill(false);
 
-    let hitPoints = new Array(length).fill(false, 0);
-
-    function hit(pos) {
-        if(pos > length || pos < 0) return;
-        hitPoints[pos] = true;
+    function hit(shipPos) {
+        hitPoints[shipPos] = true;
     }
 
     function isSunk() {
-        return hitPoints.every(pos => pos);
+        return hitPoints.every(pos => pos === true);
     }
 
     return {
-        hitPoints,
-        length,
+        get length() {
+            return length;
+        },
+        get hitPoints() {
+            return hitPoints;
+        },
         hit,
         isSunk
-    }
+    };
 }
 
 export default Ship;
